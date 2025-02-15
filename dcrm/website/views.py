436ffb6ee7c2c08e37fbe062a8c98af58a1759ac
Mufil Rahman A <mufil.r@simplicontract.com>
2025-02-16 03:47:03 +0530

    login and logout user

--- dcrm/website/views.py
+++ dcrm/website/views.py
@@ -1,4 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 def home(request):
-    return render(request,'home.html',{})
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request,username=username,password=password)
+        if user is not None:
+            login(request,user)
+            messages.success(request,'You have successfully logged in')
+            return redirect('home')
+        else:
+            messages.error(request,'Invalid login')
+            return redirect('home')
+    else:
+        return render(request,'home.html',{})
+
+def login_user(request):
+    pass
+
+def logout_user(request):
+    logout(request)
+    messages.success(request,'You have successfully logged out')
+    return redirect('home')
